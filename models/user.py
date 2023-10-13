260520ec8fb71a10b3642bae5c800efbdd8c356e
Edward_Odey <edwardodey04@gmail.com>
2023-10-13 07:23:46 -0700

    created more clasess

--- models/user.py
+++ models/user.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
+"""Module contain definition of the User class"""
 
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    """Class for managing user objects
+    Represent a User.
+    Attributes:
+        email (str): the email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): the last name of the user.
+    """
 
     email = ""
     password = ""
     first_name = ""
     last_name = ""
-
-    #super.__init__()
